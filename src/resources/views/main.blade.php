742915bb5f7aae347dd4ed7c0b02d3c507a292f4
raffimrg <mutekinoraffi@gmail.com>
2025-10-07 14:35:31 +0700

    re dev from conflict in controller/foldercontroller

--- src/resources/views/main.blade.php
+++ src/resources/views/main.blade.php
@@ -58,19 +58,6 @@
             </button>
         </div>
 
-        <!-- Alert (hidden by default) -->
-        {{-- <div id="alert-4" class="hidden flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50" role="alert"> --}}
-        {{-- <div id="alert-4" class="hidden fixed top-3 right-6 items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50"
-            role="alert">
-            <svg class="shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
-            <div class="ms-3 text-sm font-medium">
-                Pilih minimal 1 folder sebelum melanjutkan.
-            </div>
-        </div> --}}
-
         <ul class="grid w-full gap-6 md:grid-cols-1">
             @foreach ($folders as $folder)
                 <li>
